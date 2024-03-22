715ff0003555cad246668cb464151d38a1b60424
Balasis <giovani1994a@gmail.com>
2024-03-22 02:23:26 +0200

    changed comment

--- src/AppStart/GameObserver.java
+++ src/AppStart/GameObserver.java
@@ -1,6 +1,6 @@
 package AppStart;
-//The initial idea was to put observers but it avoid it since it's
-//a concept a barely even heard it. I used extend instead but kept the interface...for no reason ;p
+//The initial idea was to put observers I avoid it since it's
+//a concept I barely know. I used extend instead but kept the interface...for no reason ;p
 public interface GameObserver {
     void onGameOver();
     void redisplayFlagNumber();
