30294911f35377168ad2d12c0bdac35af160e0a2
Balasis <giovani1994a@gmail.com>
2024-03-22 02:43:09 +0200

    removed the only static I had

--- src/AppStart/Minesweeper.java
+++ src/AppStart/Minesweeper.java
@@ -5,13 +5,9 @@ import GUI.GUIInitializer;
 import javax.swing.*;
 
 public class Minesweeper {
-    private static GUIInitializer guiInitializer;
     public static void main(String[] args) {
-       guiInitializer = new GUIInitializer();
+        GUIInitializer guiInitializer = new GUIInitializer();
         SwingUtilities.invokeLater(() -> guiInitializer.run());
     }
 
-    public static GUIInitializer getGuiInitializer() {
-        return guiInitializer;
-    }
 }
